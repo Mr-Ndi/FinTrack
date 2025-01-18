5e99cb5cf611c11a1a6b0bf90039e09397aac4cc
Mr-Ndi <onlythenotes@gmail.com>
2025-01-18 14:14:30 +0200

    before serving

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello, FinTrack backend!');
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser"
+import cors from "cors"
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(bodyParser.json())
+app.use(cors());
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is http://localhost:${PORT}`)
+})
