b557f848d406b9be79a599508f647d9d4e68923d
Mr-Ndi <onlythenotes@gmail.com>
2025-01-19 20:45:51 +0200

    navbar initiation

--- fancy-fintracker/src/components/Navbar.tsx
+++ fancy-fintracker/src/components/Navbar.tsx
@@ -1,16 +1,22 @@
-import { Link } from "react-router-dom";
+import React from "react";
+import "./Navbar.css";
+import { useNavigate } from "react-router-dom";
 
-const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <div className="navbar-brand">Fancy FinTracker</div>
-            <div className="navbar-links">
-                <Link to="/">Home</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/dashboard">Dashboard</Link>
-            </div>
-        </nav>
-    );
+const NavBar: React.FC = () => {
+  const navigate = useNavigate();
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo" onClick={() => navigate("/")}>
+        FinTracker
+      </div>
+      <ul className="navbar-links">
+        <li onClick={() => navigate("/")}>Home</li>
+        <li onClick={() => navigate("/dashboard")}>Dashboard</li>
+        <li onClick={() => navigate("/login")}>Login</li>
+      </ul>
+    </nav>
+  );
 };
 
-export default Navbar;
+export default NavBar;
