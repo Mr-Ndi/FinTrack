ac55f86a6991aed0f8634b447fe9bcc481daadeb
Mr-Ndi <onlythenotes@gmail.com>
2025-01-19 22:59:53 +0200

    cors in the server

--- back-end/index.ts
+++ back-end/index.ts
@@ -7,7 +7,14 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.json())
-app.use(cors());
+
+const corsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204
+};
+app.use(cors(corsOptions));
 app.use(morgan('dev'))
 
 app.use("/fintracker",userRoute)
